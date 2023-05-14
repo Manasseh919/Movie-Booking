72a1fe111d7dcf178aa0106e4bfe13dc17e8ddbe
Manasseh919 <69004938+Manasseh919@users.noreply.github.com>
2023-05-14 22:25:38 +0000

    uselayout effect for home screen started

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,14 +1,34 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { Pressable, StyleSheet, Text, View } from "react-native";
+import React, { useLayoutEffect } from "react";
+import { useNavigation } from "@react-navigation/native";
+import { Ionicons } from "@expo/vector-icons";
 
 const HomeScreen = () => {
+  const navigation = useNavigation();
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerLeft: () => <Text>Hello Manasseh</Text>,
+      headerStyle: {
+        backgroundColor: "#f5f5f5",
+        shadowOpacity: 0.3,
+        shadowOffset: { width: -1, height: 1 },
+        shadowRadius: 3,
+      },
+      headerRight: () => (
+        <Pressable style={{ flexDirection: "row", alignItems: "center", gap: 4 }}>
+          <Ionicons name="notifications-outline" size={24} color="black" />
+          <Ionicons name="ios-location-outline" size={24} color="black" />
+        </Pressable>
+      ),
+    });
+  });
   return (
     <View>
       <Text>HomeScreen</Text>
     </View>
-  )
-}
+  );
+};
 
-export default HomeScreen
+export default HomeScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
